7370fa5f0afbc95f88487e705ef76a343b60efc6
lwilkvoich <lwilkovich@gmail.com>
2018-09-28 23:48:14 -0400

    Additions to generateMap()

--- Assets/HexMap.cs
+++ Assets/HexMap.cs
@@ -12,13 +12,27 @@ public class HexMap : MonoBehaviour {
 	public GameObject HexPrefab;
 
 	public void generateMap() {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 5; j++) {	
-				// if () {
-					
-				// }
+		for (float i = 0; i < 10; i++) {
+			for (float j = 0; j < 10; j++) {
+				float locationX = i;
+				float locationY = j;
+				float radius = 1f;
+				float height = 2;
+				float width = ((Mathf.Sqrt(3) / 2) * 2);
+				float vert = height * 0.75f;
+				float horiz = width;
+				if (j % 2 == 1) {
+					locationX = (i * width) + (width / 2);
+					locationY = (j /2) * 3;
+					Debug.Log(j);
+					Debug.Log(locationY);
+				}
+				else { 
+					locationX = i * width;
+					locationY = j + j / 2;
+				}
 				Instantiate(HexPrefab,
-							new Vector3(i, 0, j),
+							new Vector3(locationX, 0, locationY),
 							Quaternion.identity,
 							this.transform);
 			}
